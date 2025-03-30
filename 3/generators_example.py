1e0be7707d2607fef6e65c8626e380e57aecf2de
Eduard Litvinchuk <litvinchuckeduard@gmail.com>
2025-03-30 16:42:34 +0300

    Generators example

--- 3/generators_example.py
+++ 3/generators_example.py
@@ -3,14 +3,14 @@ import random
 def generate_random_numbers():
     next_number = 1
     while True:
-        next_number = yield random.randint(1, 10) * next_number
+        next_number = yield random.randint(1, next_number)
         print(next_number)
 
 
 number_gen = generate_random_numbers()
 print(next(number_gen))
-print(number_gen.send(10))
-print(number_gen.send(10))
-print(number_gen.send(10))
-print(number_gen.send(10))
+print(number_gen.send(2))
+print(number_gen.send(3))
+print(number_gen.send(4))
+print(number_gen.send(5))
 
